dea7f5b56a73e9bbe95aa7b8bb069d8e6a3e62bc
sean sulham <sean.sulham@hotmail.com>
2021-02-04 13:46:31 -0500

    Removed default html, added FormsModule.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'basic-app';
-}
+  name = 'Sean Edmund Sulham';
+  greeting = 'Hello!  My name is ' + this.name + '.';
+} 
